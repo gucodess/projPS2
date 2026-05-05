bb4ee341953a62efbdbb15fde8ec701bd84cc4f3
LucasTkoLago007 <lag.luc2007@gmail.com>
2026-05-05 20:43:57 -0300

    sei la

---
+++ src/main/java/com/atividade/projs2/CursoRepo.java
@@ -0,0 +1,6 @@
+package com.atividade.projs2;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepo extends CrudRepository<Curso, Long> {}
+
---
+++ src/main/java/com/atividade/projs2/EstudanteRepo.java
@@ -0,0 +1,7 @@
+package com.atividade.projs2;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstudanteRepo extends CrudRepository<Estudante, Long> {}
+
+
