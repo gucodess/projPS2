b4f421e4f6047e9ffae4fe3b52d010538fa45e37
GustavoTec <gustavobonifacio006@gmail.com>
2026-05-05 20:14:38 -0300

    Criação de alguns Controladores

--- projps2/src/main/java/projps2/EmpresaController.java
+++ projps2/src/main/java/projps2/EmpresaController.java
@@ -1,5 +1,34 @@
 package projps2;
+import java.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.*;
 
+@RestController
 public class EmpresaController {
-    
+    @Autowired
+    private EmpresaRepo empresaRepo;
+    public EmpresaController(){}
+
+    @GetMapping("/api/empresas")
+    Iterable<Empresa> getEmpresa() {return empresaRepo.findAll(); }
+
+    @GetMapping("/api/empresas/{id}")
+    Optional<Empresa> getEmpresa(@PathVariable long id) {return empresaRepo.findById(id); }
+
+    @PostMapping("/api/empresas")
+    Empresa createEmpresa(@RequestBody Empresa e) {return empresaRepo.save(e); }
+
+    @PutMapping("/api/empresas/{id}")
+    Optional<Empresa> updateEmpresa(@RequestBody Empresa empresa, @PathVariable long id) {
+        Optional<Empresa> opt = this.getEmpresa(id);
+        if(opt.isPresent() && opt.get().getId() == empresa.getId()){
+            return Optional.of(empresaRepo.save(empresa));
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Erro ao alterar dados da empresa com id " + id);
+    }
+
+    @DeleteMapping(value = "/api/empresas/{id}")
+    void deleteEmpresa(@PathVariable long id) {empresaRepo.deleteById(id);}
 }
