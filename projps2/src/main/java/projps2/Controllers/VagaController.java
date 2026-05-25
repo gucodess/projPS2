7eaf1b00b88f66a58af25e35f31deef036a5874c
Lucas Takao Lago <ltakaolago@gmail.com>
2026-05-25 16:14:50 -0300

    adiciona regra de negocio em vaga

--- projps2/src/main/java/projps2/Controllers/VagaController.java
+++ projps2/src/main/java/projps2/Controllers/VagaController.java
@@ -6,11 +6,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.*;
 
 import projps2.Entidades.Vaga;
+import projps2.Repositorios.EmpresaRepo;
 import projps2.Repositorios.VagaRepo;
 
+@RestController
 public class VagaController {
     @Autowired
     private VagaRepo vagaRepo;
+
+    @Autowired
+    private EmpresaRepo empresaRepo;
+
     public VagaController(){}
 
     @GetMapping("/api/vagas")
@@ -28,14 +34,33 @@ public class VagaController {
     Optional<Vaga> getVaga(@PathVariable long id) {return vagaRepo.findById(id); }
 
     @PostMapping("/api/vagas")
-    Vaga createVaga(@RequestBody Vaga v) {return vagaRepo.save(v); }
+    Vaga createVaga(@RequestBody Vaga v) {
+        if (!empresaRepo.existsById(v.getIdEmpresa())) {
+            throw new ResponseStatusException(
+                HttpStatus.CONFLICT,
+                "Não é permitido cadastrar vaga com idEmpresa inexistente."
+            );
+        }
+
+        return vagaRepo.save(v);
+    }
 
     @PutMapping("/api/vagas/{id}")
     Optional<Vaga> updateVaga(@RequestBody Vaga vaga, @PathVariable long id) {
         Optional<Vaga> opt = this.getVaga(id);
+
         if(opt.isPresent() && opt.get().getId() == vaga.getId()){
+
+            if (!empresaRepo.existsById(vaga.getIdEmpresa())) {
+                throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Não é permitido atualizar vaga com idEmpresa inexistente."
+                );
+            }
+
             return Optional.of(vagaRepo.save(vaga));
         }
+
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Erro ao alterar dados da vaga de id " + id);
     }
 
